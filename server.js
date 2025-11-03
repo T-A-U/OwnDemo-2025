2ef3f67dd64ee101278d37710d2514629854aa5d
T-A-U <153134801+T-A-U@users.noreply.github.com>
2025-11-03 18:31:05 -0500

    Database now shows in DOM but thumbs broke

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const { title } = require('process');
 const MongoClient = require('mongodb').MongoClient
 
 var db, collection;
 
-const url = "mongodb+srv://T-A-U:UqIHRL9mqMXbANuL@cluster0.htcpnep.mongodb.net/?appName=Cluster0";
-const dbName = "demo";
+const url = "mongodb+srv://T-A-U:qeoft4RRJB5oIzsj@cluster0.htcpnep.mongodb.net/?appName=Cluster0";
+const dbName = "OwnDemoGames";
 
 app.listen(3000, () => {
     MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
@@ -22,16 +23,23 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(express.static('public')) // what we put in the public folder we don't need  route it, this line handles it.
-
+//justin figured out how to push the titles and helped with setting up the structures of the database collection. He also 
 app.get('/', (req, res) => {
-  db.collection('messages').find().toArray((err, result) => {
+  db.collection('RandomGames').find().toArray((err, result) => {
     if (err) return console.log(err)
+    let titles = []
+    result.forEach((e,i) => {
+      titles.push(result[i]['title'])
+    })
+    
     res.render('index.ejs', {messages: result})
   })
 })
 
 app.post('/messages', (req, res) => {
-  db.collection('messages').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0}, (err, result) => {
+  db.collection('RandomGames').insertOne({name: req.body.name, msg: req.body.msg, thumbUp: 0, thumbDown:0},
+    // upsert: true  
+    (err, result) => {
     if (err) return console.log(err)
     console.log('saved to database')
     res.redirect('/')
@@ -39,6 +47,7 @@ app.post('/messages', (req, res) => {
 })
 
 app.put('/messages', (req, res) => {
+  console.log(req.body)
   let thumbLogic //added variable for thumbLogic
   // if( req.body.thumbDown){
   if(Object.keys(req.body)[2] == 'thumbDown'){//previous version didnt work, changed to object.keys
@@ -48,8 +57,8 @@ app.put('/messages', (req, res) => {
   }else if(Object.keys(req.body)[2]=='thumbUp'){//previous version didnt work, changed to object.keys
     thumbLogic =req.body.thumbUp +1 //plus 1 for thumb up in dom
   }
-  db.collection('messages')
-  .findOneAndUpdate({name: req.body.name, msg: req.body.msg}, {
+  db.collection('RandomGames')
+  .findOneAndUpdate({title: req.body.title}, {
     $set: {
       // thumbUp:req.body.thumbUp + 1
       //changing previous line by commenting it out
@@ -65,7 +74,7 @@ app.put('/messages', (req, res) => {
 })
 
 app.delete('/messages', (req, res) => {
-  db.collection('messages').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
+  db.collection('RandomGames').findOneAndDelete({name: req.body.name, msg: req.body.msg}, (err, result) => {
     if (err) return res.send(500, err)
     res.send('Message deleted!')
   })
